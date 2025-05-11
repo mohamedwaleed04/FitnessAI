b5517c2f0f072aea9fc51d4bd4451e0468a57cf5
mohamedwaleedcb10@gmail.com <mohamedwaleedcb10@gmail.com>
2025-05-11 16:45:41 +0300

    Final commit

--- src/modules/motion/motion.router.js
+++ src/modules/motion/motion.router.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import { 
+  analyzePose,
+} from './motion.controller.js';
 import { isAuthenticated } from '../../middleware/auth.middleware.js';
-import { videoUpload } from '../../middleware/upload.middleware.js'; 
+import { videoUpload } from '../../middleware/upload.middleware.js';
 
 const router = express.Router();
 
-// Standard analysis
 router.post('/motion/analyze', 
   isAuthenticated,
   videoUpload,
   analyzePose
 );
-export default router;
